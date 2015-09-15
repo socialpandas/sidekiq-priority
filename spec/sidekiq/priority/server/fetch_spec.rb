a49850d28cac213db3e7ca3f071f454da0cab9dd
Dávid Halász <skateman@skateman.eu>
2015-09-15 08:46:21 +0200

    Refactored spec for Sidekiq::Priority::Server::Fetch

--- spec/sidekiq/priority/server/fetch_spec.rb
+++ spec/sidekiq/priority/server/fetch_spec.rb
@@ -3,37 +3,41 @@ require 'sidekiq/priority/server/fetch'
 
 describe Sidekiq::Priority::Server::Fetch do
   describe '#initialize' do
-    before :all do
-      options = {
-        queues: ['foo', 'bar', 'foo'],
-        strict: true
-      }
-      @fetch = Sidekiq::Priority::Server::Fetch.new(options)
+    subject { Sidekiq::Priority::Server::Fetch.new(options) }
+
+    let(:options) { { queues: %w(foo bar foo), strict: true } }
+
+    let(:qs) do
+      %w(
+        queue:foo_high
+        queue:bar_high
+        queue:foo_high
+        queue:foo
+        queue:bar
+        queue:foo
+        queue:foo_low
+        queue:bar_low
+        queue:foo_low
+      )
+    end
+
+    let(:uqs) do
+      %w(
+        queue:foo_high
+        queue:bar_high
+        queue:foo
+        queue:bar
+        queue:foo_low
+        queue:bar_low
+      )
     end
 
     it 'sets queues' do
-      @fetch.instance_variable_get(:@queues).should == [
-        'queue:foo_high',
-        'queue:bar_high',
-        'queue:foo_high',
-        'queue:foo',
-        'queue:bar',
-        'queue:foo',
-        'queue:foo_low',
-        'queue:bar_low',
-        'queue:foo_low'
-      ]
+      expect(subject.instance_variable_get(:@queues)).to match_array(qs)
     end
 
     it 'sets unique_queues' do
-      @fetch.instance_variable_get(:@unique_queues).should == [
-        'queue:foo_high',
-        'queue:bar_high',
-        'queue:foo',
-        'queue:bar',
-        'queue:foo_low',
-        'queue:bar_low'
-      ]
+      expect(subject.instance_variable_get(:@unique_queues)).to match_array(uqs)
     end
   end
 end
